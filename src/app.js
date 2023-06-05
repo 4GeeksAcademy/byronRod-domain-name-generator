2dc66f394ca9c75157f280710fd042157e2a4d33
Byron Rodriguez <34363257+kitsunemura@users.noreply.github.com>
2023-06-05 21:56:24 +0000

    domain generator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,58 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const extensions = [".com", ".net", ".us", ".org", ".app"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function generateDomain() {
+  let pronoun = [
+    "the",
+    "our",
+    "everthing",
+    "few",
+    "many",
+    "most",
+    "his",
+    "her"
+  ];
+  let adj = [
+    "great",
+    "big",
+    "small",
+    "adorable",
+    "srong",
+    "happy",
+    "bright",
+    "attractive"
+  ];
+  let noun = [
+    "jogger",
+    "racoon",
+    "jelly",
+    "potato",
+    "animal",
+    "pillow",
+    "piano",
+    "pencil",
+    "paper",
+    "stone",
+    "cats"
+  ];
+
+  let randomPronoun = pronoun[Math.floor(Math.random() * pronoun.length)];
+  let randomAdj = adj[Math.floor(Math.random() * adj.length)];
+  let randomNoun = noun[Math.floor(Math.random() * noun.length)];
+  let randowmExt = extensions[Math.floor(Math.random() * extensions.length)];
+
+  return "www." + randomPronoun + randomAdj + randomNoun + randowmExt;
+}
+
+function displayDomain() {
+  const domainContainer = document.getElementById("domain-name");
+  const domain = generateDomain();
+  domainContainer.textContent = domain;
+}
+
+const generateButton = document.getElementById("btn-gen");
+generateButton.addEventListener("click", displayDomain);
+
+displayDomain();
